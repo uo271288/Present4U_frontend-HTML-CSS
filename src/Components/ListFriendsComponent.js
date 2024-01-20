e36cd04dc4e1d8d2e4fa7664e4b1cca2144e66de
uo271288 <uo271288@uniovi.es>
2024-01-20 18:38:22 +0100

    Updated List Friends to implement lists

--- src/Components/ListFriendsComponent.js
+++ src/Components/ListFriendsComponent.js
@@ -6,35 +6,53 @@ let ListFriendsComponent = () => {
 
     let [friends, setFriends] = useState([])
     let email = useRef("")
+    let listName = useRef("")
     let [message, setMessage] = useState([])
 
     useEffect(() => {
         getFriends()
-    }, [])
+    })
 
     let getFriends = async () => {
         let response = await fetch(backendURL + "/friends?apiKey=" + localStorage.getItem("apiKey"))
 
         if (response.ok) {
             let jsonData = await response.json()
-            setFriends(jsonData)
+            setFriends(jsonData.slice().sort((a, b) => {
+                return a['listName'].localeCompare(b['listName'])
+            }))
         }
     }
 
-    let deleteFriend = async (id) => {
-        await fetch(backendURL + "/friends/" + id + "?apiKey=" + localStorage.getItem("apiKey"), {
+    let deleteFriend = async (emailFriend, listId) => {
+        await fetch(backendURL + "/friends/" + emailFriend + "/" + listId + "?apiKey=" + localStorage.getItem("apiKey"), {
             method: "DELETE"
         })
-
-        getFriends()
     }
 
     let addFriend = async () => {
+        let listResponse = await fetch(backendURL + "/lists/name/" + listName.current.value + "?apiKey=" + localStorage.getItem("apiKey"))
+        let listId
+        if (listResponse.ok) {
+            let jsonData = await listResponse.json()
+            listId = jsonData.id
+        } else {
+            let jsonData = await listResponse.json()
+            if (Array.isArray(jsonData.error)) {
+                setMessage(jsonData.error)
+            } else {
+                let finalError = []
+                finalError.push(jsonData.error)
+                setMessage(finalError)
+            }
+        }
+
         let response = await fetch(backendURL + "/friends?apiKey=" + localStorage.getItem("apiKey"), {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
-                email: email.current.value
+                email: email.current.value,
+                idList: listId
             })
         })
         if (!response.ok) {
@@ -49,6 +67,7 @@ let ListFriendsComponent = () => {
         }
 
         email.current.value = ""
+        listName.current.value = ""
         getFriends()
     }
 
@@ -57,22 +76,26 @@ let ListFriendsComponent = () => {
             <h2>My friends</h2>
             {message != null && message.map(e => { return <p className="errorMessage">{e}</p> })}
             {friends.length <= 0 && <h3 className="errorMessage">No friends</h3>}
-            {friends.length > 0 && <table>
-                <tr>
-                    <th>Email</th>
-                    <th></th>
-                </tr>
+            {friends.length > 0 && <div>
                 {friends.map(friend => (
-                    <tr>
-                        <td>{friend.emailFriend}</td>
-                        <td><Link to="/listFriends"><img alt="delete" onClick={() => deleteFriend(friend.emailFriend)} src="redCross.png" /></Link></td>
-                    </tr>
+                    <dl>
+                        <dt>{friend.listName}</dt>
+                        <dd>{friend.emailFriend}
+                            <Link to="/listFriends" style={{ float: "right" }}>
+                                <img alt="delete" onClick={() => {
+                                    deleteFriend(friend.emailFriend, friend.listId)
+                                }} src="redCross.png" />
+                            </Link>
+                        </dd>
+                        <hr style={{borderTop: "2px dotted #aaa"}}/>
+                    </dl>
                 ))
                 }
-            </table>
+            </div>
             }
             <div className='friend-form-group'>
-                <input ref={email} className="add-friend-input" type='text' placeholder='Email'/>
+                <input ref={email} className="add-friend-input" type='text' placeholder='Email' />
+                <input ref={listName} className="add-friend-input" type='text' placeholder='Wishlist' />
                 <button className="add-friend-button" onClick={addFriend}>Add friend</button>
             </div>
         </div>
